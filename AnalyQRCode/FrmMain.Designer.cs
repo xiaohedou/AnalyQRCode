9c40dd3d89afd90f0b3139517e68fd5ef4c03d72
xiaohedou <wangxiaoke68@163.com>
2018-12-07 15:35:34 +0800

    添加项目文件。

---
+++ AnalyQRCode/FrmMain.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace AnalyQRCode
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "PDF文档|*.pdf|所有文件|*.*";
+            this.openFileDialog1.Title = "选择文档路径";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 548);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(930, 22);
+            this.statusStrip1.TabIndex = 11;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(215, 17);
+            this.toolStripStatusLabel2.Text = "|| Copyright ©  2017 【作者：小科】";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listView1);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listView2);
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
+            this.splitContainer1.Size = new System.Drawing.Size(930, 548);
+            this.splitContainer1.SplitterDistance = 443;
+            this.splitContainer1.TabIndex = 12;
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView1.Location = new System.Drawing.Point(12, 131);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(420, 414);
+            this.listView1.TabIndex = 9;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "图片列表";
+            this.columnHeader1.Width = 600;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.ForeColor = System.Drawing.Color.Blue;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(420, 159);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "PDF路径设置";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.ForeColor = System.Drawing.Color.Red;
+            this.button3.Location = new System.Drawing.Point(137, 66);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 36);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "提取图片";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(114, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(228, 21);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(25, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "PDF文档路径：";
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(348, 32);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(43, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "选择";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView2
+            // 
+            this.listView2.AllowColumnReorder = true;
+            this.listView2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView2.FullRowSelect = true;
+            this.listView2.GridLines = true;
+            this.listView2.Location = new System.Drawing.Point(3, 55);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(473, 490);
+            this.listView2.TabIndex = 11;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            this.listView2.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.listView2_ColumnClick);
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "图片名";
+            this.columnHeader2.Width = 130;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "二维码编号";
+            this.columnHeader3.Width = 220;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "是否重复";
+            this.columnHeader4.Width = 130;
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(3, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 37);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "识别二维码";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(930, 570);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.statusStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "二维码识别器 V1.0";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmMain_FormClosing);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ AnalyQRCode/FrmMain.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using ZXing;
+using Spire.Pdf;
+using System.Drawing.Imaging;
+
+namespace AnalyQRCode
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        string strPath = Application.StartupPath + "\\Temp\\";//设置临时文件路径
+
+        /// <summary>
+        /// 读取二维码
+        /// 读取失败，返回空字符串
+        /// </summary>
+        /// <param name="filename">指定二维码图片位置</param>
+        static string Read(string filename)
+        {
+            BarcodeReader reader = new BarcodeReader();
+            reader.Options.CharacterSet = "UTF-8";
+            Bitmap map = new Bitmap(filename);
+            Result result = reader.Decode(map);
+            map.Dispose();//释放图片文件占用资源
+            if(result==null)//如果不是二维码图片，则删除
+                File.Delete(filename);
+            return result == null ? "" : result.Text;
+        }
+        //识别二维码
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listView1.Items.Count > 0)//判断是否存在要对应的图片列表项
+            {
+                listView2.Items.Clear();//清空对比列表
+                string filename, result, flag = "";//要比较的图片文件名\图片对应二维码地址\是否重复
+                for (int i = 0; i < listView1.Items.Count; i++)//遍历所有要比较的图片列表
+                {
+                    filename = listView1.Items[i].Text;//记录当前要比较的图片文件名
+                    result = Read(filename);//获取图片对应二维码
+                    foreach (ListViewItem item in listView2.Items)//遍历对比列表中的项
+                    {
+                        if (item.SubItems[1].Text == result)//判断是否有重复的二维码地址
+                        {
+                            flag = "是(" + item.SubItems[0].Text + ")";//设置重复标识，并提示与哪一项重复
+                            break;//跳出循环
+                        }
+                        else
+                            flag = "";//设置不重复标识
+                    }
+                    if (result != "")//如果存在对应二维码地址，则添加到对比列表中
+                    {
+                        //使用二维码编号生成列表子项
+                        ListViewItem lvItem = new ListViewItem(new string[] { filename.Substring(filename.LastIndexOf('\\') + 1), result.Substring(result.LastIndexOf('\\') + 1), flag });
+                        listView2.Items.Add(lvItem);//将列表子项添加到对比列表中
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("请确认存在要识别的二维码图片列表！", "温馨提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        //选择PDF文档路径
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                textBox1.Text = openFileDialog1.FileName;
+        }
+        //提取图片
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                System.Threading.ThreadPool.QueueUserWorkItem(//使用线程池
+                     (P_temp) =>
+                     {
+                         button3.Enabled = false;
+                         button1.Enabled = false;
+                         PdfDocument doc = new PdfDocument();
+                         doc.LoadFromFile(textBox1.Text);
+
+                         #region 使用字典存储所有图片及对应页码
+                         Dictionary<Image, string> images = new Dictionary<Image, string>();
+                         for (int i = 0; i < doc.Pages.Count; i++)
+                         {
+                             if (doc.Pages[i].ExtractImages() != null)
+                             {
+                                 foreach (Image image in doc.Pages[i].ExtractImages())
+                                 {
+                                     images.Add(image, (i+1).ToString("000"));
+                                 }
+                             }
+                         }
+                         #endregion
+
+                         /*
+                          * 使用foreach遍历所有图片，并添加到List集合中
+                         IList<Image> images = new List<Image>();
+                         foreach (PdfPageBase page in doc.Pages)
+                         {
+                             if (page.ExtractImages() != null)
+                             {
+                                 foreach (Image image in page.ExtractImages())
+                                 {
+                                     images.Add(image);
+                                 }
+                             }
+                         }
+                         */
+                         doc.Close();
+
+                         int index = 1;//图片编号
+                         string page, tempPage = "1";//当前页码/上一页页码，为了图片重新编号
+                         Image tempImage;//当前图片
+                         string imageFileName,imageID;//要保存的图片文件名/图片编号
+                         #region 遍历字典中存储的所有图片及对应页码，并保存为图片
+                         foreach (var image in images)
+                         {
+                             page = image.Value;
+                             tempImage = image.Key;
+                             if (page != tempPage)
+                                 index = 1;
+                             imageID = index++.ToString("000");
+                             imageFileName = String.Format(strPath + "第" + page + "页-{0}.png", imageID);
+                             tempImage.Save(imageFileName, ImageFormat.Png);
+                             tempImage.Dispose();
+                             tempPage = page;
+                         }
+                         #endregion
+
+                         /*
+                          * 遍历List集合中的所有图片
+                         foreach (Image image in images)
+                         {
+                             String imageFileName = String.Format(strPath + "Image-{0}.png", index++.ToString("000"));
+                             image.Save(imageFileName, ImageFormat.Png);
+                         }
+                         */
+                         listView1.Items.Clear();//清空文件列表
+                         DirectoryInfo dir = new DirectoryInfo(strPath);//获取缓存文件路径
+                         FileSystemInfo[] files = dir.GetFiles();//获取文件夹中所有文件
+                         foreach (FileInfo file in files)//遍历所有文件
+                         {
+                             if (file.Extension.ToLower() == ".png")//如果是图片文件
+                             {
+                                 listView1.Items.Add(file.FullName);//显示文件列表
+                             }
+                         }
+                         button3.Enabled = true;
+                         button1.Enabled = true;
+                     });
+            }
+            else
+            {
+                MessageBox.Show("请选择PDF文档路径！", "温馨提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                button2.Focus();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "当前时间：" + DateTime.Now;//实时显示当前系统时间
+        }
+
+        private void FrmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //清空缓存文件
+            DirectoryInfo dinfo = new DirectoryInfo(Application.StartupPath + "\\Temp\\");
+            foreach (FileInfo f in dinfo.GetFiles())
+                f.Delete();
+        }
+        //列表排序
+        private void listView2_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            if (this.listView2.Columns[e.Column].Tag == null)
+                this.listView2.Columns[e.Column].Tag = true;
+            bool flag = (bool)this.listView2.Columns[e.Column].Tag;
+            if (flag)
+                this.listView2.Columns[e.Column].Tag = false;
+            else
+                this.listView2.Columns[e.Column].Tag = true;
+            this.listView2.ListViewItemSorter = new ListViewSort(e.Column, this.listView2.Columns[e.Column].Tag);
+            this.listView2.Sort();//对列表进行自定义排序  
+        }
+    }
+}
